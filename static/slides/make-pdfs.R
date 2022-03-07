020fb7cfcf6ff15aad1439e0a9e2bb306bc3c44e
Ryan Safner <ryansafner@gmail.com>
2022-03-07 14:32:09 -0500

    3.1 pdf

--- static/slides/make-pdfs.R
+++ static/slides/make-pdfs.R
@@ -1,10 +1,15 @@
 # make-pdfs
-pagedown::chrome_print(input = "https://publicS22.classes.ryansafner.com/slides/2.4-slides.html",
-                       output = "static/slides/2.4-slides.pdf",
+pagedown::chrome_print(input = "https://publicS22.classes.ryansafner.com/slides/3.1-slides.html",
+                       output = "static/slides/3.1-slides.pdf",
                        timeout = 300)
 
 # create png of first slide
-pagedown::chrome_print(input = "https://publicS22.classes.ryansafner.com/sli
+pagedown::chrome_print(input = "https://publicS22.classes.ryansafner.com/3.1-slides.html",
+                       output = "static/slides/3.1-slides.png",
+                       selector = "div.remark-slide",
+                       format = "png",
+                       timeout = 300)
+
                        
                        
                        
